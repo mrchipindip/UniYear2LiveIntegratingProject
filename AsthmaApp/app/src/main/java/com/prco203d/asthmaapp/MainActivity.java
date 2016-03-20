60e34825578a376c1931da93c1c7988e1f645b3c
Sam James Booty <sbooty17@gmail.com>
2016-03-20 16:52:53 +0000

    Added ability for user to save thier peak flow and updated the main page Ui

--- AsthmaApp/app/src/main/java/com/prco203d/asthmaapp/MainActivity.java
+++ AsthmaApp/app/src/main/java/com/prco203d/asthmaapp/MainActivity.java
@@ -1,22 +1,47 @@
 package com.prco203d.asthmaapp;
 
 import android.content.Intent;
+import android.nfc.Tag;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, View.OnKeyListener {
+
+
+    private EditText peakFlowVariableEditText = null;
+    private Button submitButton = null;
+
+    public float peakFlowToday;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        Toolbar toolbar          = (Toolbar) findViewById(R.id.toolbar);
+        submitButton             = (Button) findViewById(R.id.submitButton);
+        peakFlowVariableEditText = (EditText) findViewById(R.id.peakFlowVariableEditText);
+
+        submitButton.setOnClickListener(this);
+        peakFlowVariableEditText.setOnKeyListener(this);
+
+
         setSupportActionBar(toolbar);
 
     }
@@ -77,4 +102,32 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, AppSettingsActivity.class);
         startActivity(intent);
     }
-}
+
+    @Override
+    public void onClick(View v) {
+
+        try
+        {
+            peakFlowToday = Float.valueOf(peakFlowVariableEditText.getText().toString());
+
+            Calendar c = Calendar.getInstance();            //Not sure if this bit actually does anything Nick?
+            int dateTime = c.get(Calendar.SECOND);
+
+
+        }
+        catch (NumberFormatException ex)
+        {
+            Toast.makeText(this, R.string.no_value, Toast.LENGTH_SHORT).show();
+        }
+
+        }
+
+    @Override
+    public boolean onKey(View v, int keyCode, KeyEvent event) {
+        return false;
+    }
+
+
+
+    }
+
