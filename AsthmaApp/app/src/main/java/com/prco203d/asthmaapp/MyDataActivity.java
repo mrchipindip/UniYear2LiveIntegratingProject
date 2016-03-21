468a5897ccaf88a23027d42a00c0776b52003871
Nick Wade <nicholas.wade@students.plymouth.ac.uk>
2016-03-21 17:03:10 +0000

    Made Setup page write to preferences file, and My Data file reflect the changes.

--- AsthmaApp/app/src/main/java/com/prco203d/asthmaapp/MyDataActivity.java
+++ AsthmaApp/app/src/main/java/com/prco203d/asthmaapp/MyDataActivity.java
@@ -1,13 +1,19 @@
 package com.prco203d.asthmaapp;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.TextView;
 
 public class MyDataActivity extends AppCompatActivity {
+
+    private TextView textTitle = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +23,27 @@ public class MyDataActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        textTitle = (TextView) findViewById(R.id.textViewTitle);
+
+        SharedPreferences sharedPrefs = getSharedPreferences("UserData", Context.MODE_PRIVATE);
+        String name = sharedPrefs.getString("Name", "");
+
+        textTitle.setText(name + "'s Data");
+
+
+    }
+
+  //  @Override
+//    protected void onStart() {
+//        super.onStart();
+
+//    }
+
+
+    public void openSetup(View view) {
+        Intent intent = new Intent(this, SetupActivity.class);
+        startActivity(intent);
     }
 
 }
--- AsthmaApp/app/src/main/java/com/prco203d/asthmaapp/SetupActivity.java
+++ AsthmaApp/app/src/main/java/com/prco203d/asthmaapp/SetupActivity.java
@@ -1,13 +1,19 @@
 package com.prco203d.asthmaapp;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.EditText;
 
 public class SetupActivity extends AppCompatActivity {
+
+    private EditText editTextName = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +22,23 @@ public class SetupActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        editTextName = (EditText) findViewById(R.id.editTextName);
+
+    }
+
+    public void submitData(View view) {
+
+        // write the data to a pref file
+        SharedPreferences sharedPrefs = getSharedPreferences("UserData", Context.MODE_PRIVATE);
+
+        SharedPreferences.Editor editor = sharedPrefs.edit();
+
+        editor.putString("Name", editTextName.getText().toString());
+        editor.apply();
+
+        // Move user to a different page
+        Intent intent = new Intent(this, MyDataActivity.class);
+        startActivity(intent);
     }
 
 }
