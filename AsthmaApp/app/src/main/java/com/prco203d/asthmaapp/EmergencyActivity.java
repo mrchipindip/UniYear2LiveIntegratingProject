74db00816bb53040aad6679743e5804702f88da6
Christopher Wesemael <chrislbw@hotmail.co.uk>
2016-03-22 02:53:17 +0000

    Added Emergency page components into scroll view. Added all of the components for the puff timer. Added The code to make the puff timer work (disables button, changes textview at correct time). Still need to change timer variables to appropriate numbers. and make last button work

--- AsthmaApp/app/src/main/java/com/prco203d/asthmaapp/EmergencyActivity.java
+++ AsthmaApp/app/src/main/java/com/prco203d/asthmaapp/EmergencyActivity.java
@@ -1,13 +1,25 @@
 package com.prco203d.asthmaapp;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
-public class EmergencyActivity extends AppCompatActivity {
+import java.util.concurrent.TimeUnit;
+
+public class EmergencyActivity extends AppCompatActivity implements View.OnClickListener {
+    int i = 0;
+    long sleepTime = 1;
+    int numPuffs = 0;
+    CountDownTimer cwCountDownTimer;
+    Button btnStartTimer;
+    TextView txtEmergencyCountDown;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,7 +28,65 @@ public class EmergencyActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+
+        final ProgressBar cwProgressBar = (ProgressBar) findViewById(R.id.emergencyProgressBar);
+        btnStartTimer = (Button)findViewById(R.id.emergencyStartTimer);
+        btnStartTimer.setOnClickListener(this);
+        txtEmergencyCountDown = (TextView)findViewById(R.id.emergencyCountdown);
+        cwProgressBar.setProgress(i);
+        cwCountDownTimer = new CountDownTimer(10000,1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                i++;
+                cwProgressBar.setProgress(i);
+            }
+
+            @Override
+            public void onFinish() {
+                i++;
+                cwProgressBar.setProgress(i);
+                numPuffs++;
+                txtEmergencyCountDown.setText("Puff!");
+                new CountDownTimer(2000, 1000) {
+                    public void onFinish() {
+                        EmergencyPuffTimer();
+                    }
+
+                    public void onTick(long millisUntilFinished) {
+                        // millisUntilFinished    The amount of time until finished.
+                    }
+                }.start();
+
+
+            }
+        };
+
+
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    void EmergencyPuffTimer()
+    {
+        btnStartTimer.setEnabled(false);
+        if(numPuffs < 10)
+        {
+            txtEmergencyCountDown.setText("Get Ready..");
+            i = 0;
+            cwCountDownTimer.start();
+        }
+        else
+        {
+            btnStartTimer.setEnabled(true);
+            numPuffs = 0;
+            txtEmergencyCountDown.setText("Get Ready..");
+        }
+
+    }
+    @Override
+    public void onClick(View v) {
+
+        EmergencyPuffTimer();
+
     }
 
 }
