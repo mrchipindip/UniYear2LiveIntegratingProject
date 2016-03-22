d1b0c73b8a7be713c91da85e657fc1ac4ec531e9
Sam James Booty <sam.booty@students.plymouth.ac.uk>
2016-03-22 10:50:40 +0000

    Added more strings to unwell activity, also change the imageViews over to buttons, added toasts for all the imageButtons on unwell.

--- AsthmaApp/app/src/main/java/com/prco203d/asthmaapp/UnwellActivity.java
+++ AsthmaApp/app/src/main/java/com/prco203d/asthmaapp/UnwellActivity.java
@@ -6,14 +6,58 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class UnwellActivity extends AppCompatActivity {
+
+    ImageButton symptomsImageButton = null;
+    ImageButton cantSleepImageButton = null;
+    ImageButton cantTalkImageButton = null;
+    ImageButton peakFlowDropImageButton = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_unwell);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        symptomsImageButton = (ImageButton) findViewById(R.id.symptomsImageButton);
+        cantSleepImageButton = (ImageButton) findViewById(R.id.cantSleepImageButton);
+        cantTalkImageButton = (ImageButton) findViewById(R.id.cantTalkImageButton);
+        peakFlowDropImageButton = (ImageButton) findViewById(R.id.peakFlowDropImageButton);
+
+
+        symptomsImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //startActivity(new Intent(WellActivity.this, Pop.class));
+                Toast.makeText(getApplicationContext(), R.string.symptoms_popup, Toast.LENGTH_LONG).show();
+            }
+        });
+        cantSleepImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //startActivity(new Intent(WellActivity.this, Pop.class));
+                Toast.makeText(getApplicationContext(), R.string.cant_sleep_popup, Toast.LENGTH_LONG).show();
+            }
+        });
+        cantTalkImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //startActivity(new Intent(WellActivity.this, Pop.class));
+                Toast.makeText(getApplicationContext(), R.string.cant_talk_popup, Toast.LENGTH_LONG).show();
+            }
+        });
+        peakFlowDropImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //startActivity(new Intent(WellActivity.this, Pop.class));
+                Toast.makeText(getApplicationContext(), R.string.peakflow_drop_popup, Toast.LENGTH_LONG).show();
+            }
+        });
+
+
         setSupportActionBar(toolbar);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
