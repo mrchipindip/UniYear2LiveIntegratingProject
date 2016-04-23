8e6970003290758eac06739ccfcb68422012569f
Nick Wade <nicholas.wade@students.plymouth.ac.uk>
2016-04-23 11:58:15 +0100

    More work on setup, new layout for  triggers (again) and links to background activity.

--- AsthmaApp/app/src/main/java/com/prco203d/asthmaapp/Setup5Activity.java
+++ AsthmaApp/app/src/main/java/com/prco203d/asthmaapp/Setup5Activity.java
@@ -3,6 +3,9 @@ package com.prco203d.asthmaapp;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.graphics.LightingColorFilter;
+import android.graphics.PorterDuff;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -15,6 +18,19 @@ import android.widget.EditText;
 public class Setup5Activity extends AppCompatActivity {
 
     //private EditText editPeakFlow   = null;
+    private Button buttonAnimal;
+    private Button buttonDust;
+    private Button buttonEmotion;
+    private Button buttonExercise;
+    private Button buttonFood;
+    private Button buttonPollen;
+    private Button buttonSmoke;
+    private Button buttonStress;
+    private Button buttonOdour;
+    private Button buttonVirus;
+    private Button buttonWeather;
+
+
     private Button buttonUpdate;
     private Button buttonNext;
     private Button buttonPrevious;
@@ -29,6 +45,18 @@ public class Setup5Activity extends AppCompatActivity {
 
         // Change to triggers
         //editPeakFlow = (EditText) findViewById(R.id.editTextPeak);
+
+        buttonAnimal = (Button)findViewById(R.id.buttonAnimal);
+        buttonDust = (Button)findViewById(R.id.buttonDust);
+        buttonEmotion = (Button)findViewById(R.id.buttonEmotion);
+        buttonExercise = (Button)findViewById(R.id.buttonExercise);
+        buttonFood = (Button)findViewById(R.id.buttonFoods);
+        buttonPollen = (Button)findViewById(R.id.buttonPollen);
+        buttonSmoke = (Button)findViewById(R.id.buttonSmoke);
+        buttonStress = (Button)findViewById(R.id.buttonStress);
+        buttonOdour = (Button)findViewById(R.id.buttonOdours);
+        buttonVirus = (Button)findViewById(R.id.buttonVirus);
+        buttonWeather = (Button)findViewById(R.id.buttonWeather);
 
         buttonUpdate = (Button)findViewById(R.id.buttonSubmit);
         buttonNext = (Button)findViewById(R.id.buttonNext);
@@ -48,9 +76,101 @@ public class Setup5Activity extends AppCompatActivity {
             // Show next + previous buttons only
             buttonUpdate.setVisibility(View.INVISIBLE);
         }
+        if(sharedPrefs.getBoolean("TriggerAnimal", false)){
+            buttonDust.setBackgroundColor(Color.parseColor("#8BC34A"));
+        }
+        if(sharedPrefs.getBoolean("TriggerDust", false)){
+            buttonDust.setBackgroundColor(Color.parseColor("#8BC34A"));
+        }
+        if(sharedPrefs.getBoolean("TriggerEmotion", false)){
+            buttonEmotion.setBackgroundColor(Color.parseColor("#8BC34A"));
+        }
+        if(sharedPrefs.getBoolean("TriggerExercise", false)){
+            buttonExercise.setBackgroundColor(Color.parseColor("#8BC34A"));
+        }
+        if(sharedPrefs.getBoolean("TriggerFood", false)){
+            buttonFood.setBackgroundColor(Color.parseColor("#8BC34A"));
+        }
+        if(sharedPrefs.getBoolean("TriggerPollen", false)){
+            buttonPollen.setBackgroundColor(Color.parseColor("#8BC34A"));
+        }
+        if(sharedPrefs.getBoolean("TriggerSmoke", false)){
+            buttonSmoke.setBackgroundColor(Color.parseColor("#8BC34A"));
+        }
+        if(sharedPrefs.getBoolean("TriggerStress", false)){
+            buttonStress.setBackgroundColor(Color.parseColor("#8BC34A"));
+        }
+        if(sharedPrefs.getBoolean("TriggerOdour", false)){
+            buttonOdour.setBackgroundColor(Color.parseColor("#8BC34A"));
+        }
+        if(sharedPrefs.getBoolean("TriggerVirus", false)){
+            buttonVirus.setBackgroundColor(Color.parseColor("#8BC34A"));
+        }
+        if(sharedPrefs.getBoolean("TriggerWeather", false)){
+            buttonWeather.setBackgroundColor(Color.parseColor("#8BC34A"));
+        }
 
         //int peak = (sharedPrefs.getInt("Peak", 0));
         //editPeakFlow.setText("" + peak);
+    }
+
+    public void buttonToggle(View view) {
+
+        switch (view.getId()) {
+            case R.id.buttonAnimal:
+                doStuff("Animal", buttonAnimal);
+                break;
+            case R.id.buttonDust:
+                doStuff("Dust", buttonDust);
+                break;
+            case R.id.buttonEmotion:
+                doStuff("Emotion", buttonEmotion);
+                break;
+            case R.id.buttonExercise:
+                doStuff("Exercise", buttonExercise);
+                break;
+            case R.id.buttonFoods:
+                doStuff("Food", buttonFood);
+                break;
+            case R.id.buttonPollen:
+                doStuff("Pollen", buttonPollen);
+                break;
+            case R.id.buttonSmoke:
+                doStuff("Smoke", buttonSmoke);
+                break;
+            case R.id.buttonStress:
+                doStuff("Stress", buttonStress);
+                break;
+            case R.id.buttonOdours:
+                doStuff("Odour", buttonOdour);
+                break;
+            case R.id.buttonVirus:
+                doStuff("Virus", buttonVirus);
+                break;
+            case R.id.buttonWeather:
+                doStuff("Weather", buttonWeather);
+                break;
+        }
+    }
+
+    public void doStuff(String inputName,  Button inputButton){
+
+        SharedPreferences sharedPrefs = getSharedPreferences("UserData", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPrefs.edit();
+
+        String trigger = "trigger" + inputName;
+
+
+        if(sharedPrefs.getBoolean(trigger, false)){
+            inputButton.setBackgroundColor(Color.parseColor("#8BC34A"));
+            editor.putBoolean(trigger, false);
+            editor.apply();
+        }
+        else{
+            inputButton.setBackgroundColor(Color.parseColor("#dddddd"));
+            editor.putBoolean(trigger, true);
+            editor.apply();
+        }
     }
 
     // Overriding the back key, for first-time setup
