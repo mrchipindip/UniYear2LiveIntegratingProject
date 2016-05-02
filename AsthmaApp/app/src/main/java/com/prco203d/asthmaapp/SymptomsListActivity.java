21a3c1fdfab051ea39d206cf837e1b6278e4a16d
Nick Wade <nicholas.wade@students.plymouth.ac.uk>
2016-05-02 06:40:20 +0100

    Resolved merge and spelling issues with strings.txt and added symptom headers and temp descriptions. Applied strings to symptoms activity.

--- AsthmaApp/app/src/main/java/com/prco203d/asthmaapp/SymptomsListActivity.java
+++ AsthmaApp/app/src/main/java/com/prco203d/asthmaapp/SymptomsListActivity.java
@@ -38,22 +38,49 @@ public class SymptomsListActivity extends AppCompatActivity {
         listDataHeader = new ArrayList<String>();
         listDataChild = new HashMap<String, List<String>>();
 
-        listDataHeader.add("Symptom example 1");
-        listDataHeader.add("Symptom example 2");
-        listDataHeader.add("Symptom example 3");
+        listDataHeader.add(getResources().getString(R.string.symptom_heading_breathing_difficulty));
+        listDataHeader.add(getResources().getString(R.string.symptom_heading_coughing));
+        listDataHeader.add(getResources().getString(R.string.symptom_heading_exercising_difficulty));
+        listDataHeader.add(getResources().getString(R.string.symptom_heading_talking_difficulty));
+        listDataHeader.add(getResources().getString(R.string.symptom_heading_tight_chest));
+        listDataHeader.add(getResources().getString(R.string.symptom_heading_waking));
+        listDataHeader.add(getResources().getString(R.string.symptom_heading_walking_difficulty));
+        listDataHeader.add(getResources().getString(R.string.symptom_heading_wheezing));
 
         List<String> symptom1 = new ArrayList<String>();
-        symptom1.add("Symptom 1 info");
+        symptom1.add(getResources().getString(R.string.symptom_description_breathing_difficulty));
 
         List<String> symptom2 = new ArrayList<String>();
-        symptom2.add("Symptom 2 info");
+        symptom2.add(getResources().getString(R.string.symptom_description_coughing));
 
         List<String> symptom3 = new ArrayList<String>();
-        symptom3.add("Symptom 3 info");
+        symptom3.add(getResources().getString(R.string.symptom_description_exercising_difficulty));
+
+        List<String> symptom4 = new ArrayList<String>();
+        symptom3.add(getResources().getString(R.string.symptom_description_talking_difficulty));
+
+        List<String> symptom5 = new ArrayList<String>();
+        symptom3.add(getResources().getString(R.string.symptom_description_tight_chest));
+
+        List<String> symptom6 = new ArrayList<String>();
+        symptom3.add(getResources().getString(R.string.symptom_description_waking));
+
+        List<String> symptom7 = new ArrayList<String>();
+        symptom3.add(getResources().getString(R.string.symptom_description_walking_difficulty));
+
+        List<String> symptom8 = new ArrayList<String>();
+        symptom3.add(getResources().getString(R.string.symptom_description_wheezing));
 
         listDataChild.put(listDataHeader.get(0), symptom1);
         listDataChild.put(listDataHeader.get(1), symptom2);
         listDataChild.put(listDataHeader.get(2), symptom3);
+        listDataChild.put(listDataHeader.get(3), symptom4);
+        listDataChild.put(listDataHeader.get(4), symptom5);
+        listDataChild.put(listDataHeader.get(5), symptom6);
+        listDataChild.put(listDataHeader.get(6), symptom7);
+        listDataChild.put(listDataHeader.get(7), symptom8);
+
+
     }
 
 }
